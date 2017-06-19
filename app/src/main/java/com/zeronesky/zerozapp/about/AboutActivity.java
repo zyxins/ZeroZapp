39e1c5261e37ffc578c9b8e6191c35cc0225a7e6
zyxins <zyxins@gmail.com>
2017-06-19 16:27:39 +0800

    rename package name

--- app/src/main/java/com/zeronesky/zerozapp/about/AboutActivity.java
+++ app/src/main/java/com/zeronesky/zerozapp/about/AboutActivity.java
@@ -1,10 +1,10 @@
-package com.netsky.zerozapp.about;
+package com.zeronesky.zerozapp.about;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.netsky.zerozapp.R;
+import com.zeronesky.zerozapp.R;
 import com.vansuita.materialabout.builder.AboutBuilder;
 
 /**
